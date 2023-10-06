b65d557b23284f67cf42776365ea7a35ca2825b2
Pedro Henrique <phcosta069@gmail.com>
2023-10-06 16:53:15 -0300

    feat: history page include

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,7 +1,28 @@
+import { TimerTask } from "../entities/TimerTask"
+
 export function History() {
+    const timerTasksList: TimerTask[] = JSON.parse(localStorage.getItem('timer-tasks-list') ?? '[]')
+
     return (
         <section>
             <h2>My <span>History</span></h2>
-        </section>
+
+            <table border={1}>
+                <tr>
+                    <th>Task</th>
+                    <th>Duration</th>
+                </tr>
+                {
+                    timerTasksList.map(({ id, taskName, taskMinutes }) => {
+                        return (
+                            <tr key={id}>
+                                <td>{taskName}</td>
+                                <td>{taskMinutes}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </table>
+        </section >
     )
 }
---
+++ test.js
@@ -0,0 +1,3 @@
+const today = new Date()
+
+console.log('Hoje é dia ' + new Date(today).getDate() + ' do ' + new Date(today).getMonth() + ' de ' + new Date(today).getFullYear())
