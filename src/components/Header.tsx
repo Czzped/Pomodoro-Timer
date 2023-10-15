3ab78fa7f0d10c011a5c722268b540e870f7f21e
Pedro Henrique <phcosta069@gmail.com>
2023-10-15 01:28:13 -0300

    feat: header style include

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,8 @@
-import logo from "../assets/svgs/logo.svg"
-
 export function Header() {
     return (
-        <header>
-            <img src={logo} alt="" />
+        <header className="flex flex-col justify-center items-center h-[15vh] gap-4">
+            <h1 className="text-4xl font-bold">pomodoro</h1>
+            <hr className=" w-[75vw] h-[0.4rem] bg-secondary border-none rounded-[0.125rem]" />
         </header>
     )
 }
