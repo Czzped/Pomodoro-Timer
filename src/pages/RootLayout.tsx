b72b160bd0a75d69f4e1327fe44d8eb5c68eaccc
Pedro Henrique <phcosta069@gmail.com>
2023-10-15 12:00:33 -0300

    feat: RootLayout style include

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -4,12 +4,12 @@ import { Footer } from "../components/Footer"
 
 export function RootLayout() {
     return (
-        <>
+        <div className="flex flex-col justify-between h-screen p-4">
             <Header />
             <main>
                 <Outlet />
             </main>
             <Footer />
-        </>
+        </div>
     )
 }
