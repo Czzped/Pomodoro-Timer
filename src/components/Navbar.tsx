d47e01f104ef78414b87fae6e22957721fbe3750
Pedro Henrique <phcosta069@gmail.com>
2023-10-27 22:14:18 -0300

    build: phosphor-react imgs include

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { Timer, Scroll } from 'phosphor-react'
 
 export function Navbar() {
     return (
@@ -6,12 +7,12 @@ export function Navbar() {
             <ul className="flex gap-2">
                 <li className="border-b-2 border-bgPrimary hover:border-primary">
                     <Link to="/" >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><circle cx="128" cy="128" r="88" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="16"></circle><line x1="128" y1="128" x2="167.6" y2="88.4" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="104" y1="8" x2="152" y2="8" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line></svg>
+                        <Timer size={24} />
                     </Link>
                 </li>
                 <li className="border-b-2 border-bgPrimary hover:border-primary">
                     <Link to="/history">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><path d="M200,176V64a23.9,23.9,0,0,0-24-24H40" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path><line x1="104" y1="104" x2="168" y2="104" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="104" y1="136" x2="168" y2="136" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><path d="M22.1,80A24,24,0,1,1,64,64V192a24,24,0,1,0,41.9-16h112A24,24,0,0,1,200,216H88" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path></svg>
+                        <Scroll size={24} />
                     </Link>
                 </li>
             </ul>
